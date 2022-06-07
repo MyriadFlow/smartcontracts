ce7b59decea09a0ce02e8164c023f10497694196
thisisommore <ommore501@gmail.com>
2022-06-07 07:44:20 +0000

    indexer: deploy.sh - Update block number in subgraph

--- subgraph/deploy.sh
+++ subgraph/deploy.sh
@@ -1,5 +1,9 @@
 yq eval -i '.dataSources[0].source.address = strenv(CREATIFY_ADDRESS)' subgraph.yaml
+yq eval -i '.dataSources[0].source.startBlock = strenv(CREATIFY_BLOCK_NO)' subgraph.yaml
+
 yq eval -i '.dataSources[1].source.address = strenv(MARKETPLACE_ADDRESS)' subgraph.yaml
+yq eval -i '.dataSources[1].source.startBlock = strenv(MARKETPLACE_BLOCK_NO)' subgraph.yaml
+
 
 yarn create:hosted
 yarn deploy:hosted 
