9c6a615ff45eaf680b6a141a71cf6d4f28c1b503
thisisommore <ommore501@gmail.com>
2022-06-07 08:46:50 +0000

    indexer: deploy.sh - Fix mismatch type for startBlock

--- subgraph/deploy.sh
+++ subgraph/deploy.sh
@@ -1,8 +1,8 @@
 yq eval -i '.dataSources[0].source.address = strenv(CREATIFY_ADDRESS)' subgraph.yaml
-yq eval -i '.dataSources[0].source.startBlock = strenv(CREATIFY_BLOCK_NO)' subgraph.yaml
+yq eval -i '.dataSources[0].source.startBlock = env(CREATIFY_BLOCK_NO)' subgraph.yaml
 
 yq eval -i '.dataSources[1].source.address = strenv(MARKETPLACE_ADDRESS)' subgraph.yaml
-yq eval -i '.dataSources[1].source.startBlock = strenv(MARKETPLACE_BLOCK_NO)' subgraph.yaml
+yq eval -i '.dataSources[1].source.startBlock = env(MARKETPLACE_BLOCK_NO)' subgraph.yaml
 
 
 yarn create:hosted
