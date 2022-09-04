8c2778657b2bf419111b5033c6ac871d412d1c85
thisisommore <ommore501@gmail.com>
2022-09-04 10:32:20 +0000

    dev(watch - task.sh): fix wrong script call

--- task.sh
+++ task.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ETHEREUM_RPC_URL="" NETWORK=localhost yarn deploy:local;
+ETHEREUM_RPC_URL="" NETWORK=localhost yarn deploy;
 cd subgraph && yarn codegen && yarn deploy-local -l v0.0.31 1>/dev/null
