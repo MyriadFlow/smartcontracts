d1c4bc517838f7bbc1a4f0c79d68369b3054d974
inciner8r <punarvpawade17@gmail.com>
2023-10-03 23:16:09 +0530

    api: add remaining routes

--- api/api.go
+++ api/api.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/MyriadFlow/smartcontracts/api/contracts"
+	"github.com/MyriadFlow/smartcontracts/api/roles"
+	"github.com/MyriadFlow/smartcontracts/api/subgraph"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,5 +11,7 @@ func ApplyRoutes(r *gin.Engine) {
 	api := r.Group("/api")
 	{
 		contracts.ApplyRoutes(api)
+		subgraph.ApplyRoutes(api)
+		roles.ApplyRoutes(api)
 	}
 }
